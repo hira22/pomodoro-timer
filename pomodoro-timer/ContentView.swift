337e6505028e69494e1e4b772bf4327a24d3c98d
hiraoka <m.hiraoka22@gmail.com>
2020-02-16 16:32:05 +0900

    [add] pomodoro logic. 5 minutes rest time every 25 minutes.

--- pomodoro-timer/ContentView.swift
+++ pomodoro-timer/ContentView.swift
@@ -10,7 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
     @State var timer: Timer?
-    @State var count: TimeInterval = 0.0
+    @State var count: TimeInterval = 0.0 {
+        didSet {
+            self.isRestTime = {
+                if case 0...5 = Int(count) / 60 % 60 % 25 {
+                    // TODO: for Debug
+                    // if case 0...5 = Int(count) % 60 % 25 {
+                    return true
+                }
+                return false
+            }()
+        }
+    }
     
     var timeCount: String {
         let formatter = DateComponentsFormatter()
@@ -20,22 +31,33 @@ struct ContentView: View {
         return formatter.string(from: count)!
     }
     
+    @State var isRestTime: Bool = false
+    
     var body: some View {
-        VStack {
-            Text(timeCount)
-            HStack {
-                Button("start") {
-                    self.timer?.invalidate()
-                    self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { timer in
-                        self.count += timer.timeInterval
-                    })
-                }
-                Button("reset") {
-                    self.timer?.invalidate()
-                    self.$count.wrappedValue = 0.0
-                    self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { timer in
-                        self.count += timer.timeInterval
-                    })
+        ZStack {
+            if $isRestTime.wrappedValue {
+                Color.yellow.edgesIgnoringSafeArea(.all)
+            } else {
+                Color.green.edgesIgnoringSafeArea(.all)
+            }
+            VStack {
+                Text(timeCount)
+                // TODO: for Debug
+                // Text("\(Int(count) % 60 % 5)")
+                HStack {
+                    Button("start") {
+                        self.timer?.invalidate()
+                        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { timer in
+                            self.count += timer.timeInterval
+                        })
+                    }
+                    Button("reset") {
+                        self.timer?.invalidate()
+                        self.$count.wrappedValue = 0.0
+                        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { timer in
+                            self.count += timer.timeInterval
+                        })
+                    }
                 }
             }
         }
